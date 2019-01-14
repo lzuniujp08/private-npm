ecc0823bc93e9283927c9a14f3ebc957bb735e6e
lzugis <1004740957@qq.com>
2019-01-14 17:04:31 +0800

    init

--- vue-components/src/main.js
+++ vue-components/src/main.js
@@ -2,7 +2,26 @@ import GwLegend from './components/gw-legend/index';
 import GwMessage from './components/gw-message/index';
 import GwTimeline from './components/gw-timeline/index';
 
-export {
+const components = [
+  GwLegend,
+  GwMessage,
+  GwTimeline
+];
+
+const install = function(Vue, opts = {}) {
+  components.forEach(component => {
+    Vue.component(component.name, component);
+  });
+};
+
+/* istanbul ignore if */
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+module.exports = {
+  version: '1.0.0',
+  install,
   GwLegend,
   GwMessage,
   GwTimeline
